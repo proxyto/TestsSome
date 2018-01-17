7cb799e5a2430f189a79b41754fed3dbfafed5ed
proxyto <aleks_kovachev@abv.bg>
2018-01-17 13:15:02 +0200

    new files

---
+++ Test.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+	int a, b,c;
+	std::cout << "Hi" << std::endl;
+	std::cin >> a;
+	std::cin >> b;
+	c = a + b;
+	std::cout << c << std::endl;
+	std::cin.get();
+	std::cin.get();
+}
